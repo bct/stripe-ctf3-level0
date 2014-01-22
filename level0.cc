130158f211ef938f9915cf1ac7b1a614c26526c5
Brendan Taylor <whateley@gmail.com>
2014-01-22 14:24:30 -0700

    use a binary search instead of a set.
    this saves another 10%

--- level0.cc
+++ level0.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <unordered_set>
+#include <vector>
 #include <algorithm>
 #include <string>
 #include <string.h>
@@ -10,19 +10,23 @@
 int main( int argc, char *argv[] ) {
   const char *words_path = argc > 1 ? argv[1] : "/usr/share/dict/words";
 
-  std::unordered_set<std::string> words_set (255350);
+  std::vector<std::string> words;
+  words.reserve(250000);
 
   std::ifstream words_file;
   words_file.open(words_path);
 
   std::string current_word;
 
+  int n = 0;
+
   while(!words_file.eof()) {
     getline(words_file, current_word);
 
-//    std::cout << current_word << std::endl;
+    // downcase everything, caps mean it won't be sorted by default
+    std::transform(current_word.begin(), current_word.end(), current_word.begin(), ::tolower);
 
-    words_set.insert(current_word);
+    words.push_back(current_word);
   }
 
   char whitespace;
@@ -40,7 +44,7 @@ int main( int argc, char *argv[] ) {
 
     std::transform(current_word.begin(), current_word.end(), current_word.begin(), ::tolower);
 
-    if(words_set.find(current_word) != words_set.end()) {
+    if(std::binary_search(words.begin(), words.end(), current_word)) {
       std::cout << orig_word;
     } else {
       std::cout << "<" << orig_word << ">";
