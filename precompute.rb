8a2b53315fdd1ca5795de0f95856496389ae1a16
Brendan Taylor <whateley@gmail.com>
2014-01-23 09:30:42 -0700

    shave 2MB off the binary

--- precompute.rb
+++ precompute.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env ruby
 
 # I determined that the longest word is 25chars (including terminator)
+MAX_WORD_LENGTH = 25
+
+
 words = File.readlines("words").sort.uniq
 
 File.open("precomputed.c", "w") do |output|
   output.puts "#define WORD_COUNT #{words.count}"
-  output.puts "const char words[WORD_COUNT][32] = {"
+  output.puts "#define WORD_LENGTH #{MAX_WORD_LENGTH}"
+  output.puts "const char words[WORD_COUNT][WORD_LENGTH] = {"
 
   words.each do |word|
     word = word.chomp
-    block = word + ("\\0" * (31 - word.length))
+    block = word + ("\\0" * (MAX_WORD_LENGTH - 1 - word.length))
 
     output.puts "  \"#{block}\","
   end
